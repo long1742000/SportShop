c5d6b451c73b963f28a0104709082bfca86b9f28
Pham Hoang Duy Long <phamduylong1742000@gmail.com>
2022-09-09 13:26:27 +0700

    first commit

---
+++ SportShop/src/main/java/com/aptech/Dao/UtilDb.java
@@ -0,0 +1,51 @@
+package com.aptech.Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class UtilDb {
+
+	private Connection connection;
+
+	public UtilDb(Connection conection) {
+		this.connection = conection;
+	}
+
+	UtilDb() {
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public void connect() {
+		if (connection == null) {
+//            String dbURL = "jdbc:sqlserver://localhost\\PHAMTHANHPHONG:1433;databaseName=SportShop;user=sa;password=sa";
+//            String dbURL = "jdbc:sqlserver://localhost;databaseName=SportShop;user=sa;password=123456"; //Huy
+			String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=SportShop;user=sa;password=123"; // Long
+			if (this.connection == null) {
+				try {
+					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+					connection = DriverManager.getConnection(dbURL);
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+
+	public void disConnect() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+}
